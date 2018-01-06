df0df90c854d2a656d13d333f59a73c7cab3534d
Chris Procak <cjprocak13@gmail.com>
2018-01-06 08:34:11 -0600

    Added Cris picture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import cris from './rejazzer2.png';
+
 import './App.css';
 
 class App extends Component {
@@ -8,11 +10,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	<img src={cris} alt="test"/>
       </div>
     );
   }
